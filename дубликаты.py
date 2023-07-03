82b662e9a61dc34a7063fba2d38d1100a780d2f2
annanikonorova1 <131855552+annanikonorova1@users.noreply.github.com>
2023-07-03 20:22:37 +0300

    Add files via upload

--- дубликаты.py
+++ дубликаты.py
@@ -4,7 +4,7 @@ temp = []
 for x in list(nabor_1):
     if x not in temp:
         temp.append(x)
-
+nabor_2 = {x for x in nabor_1 if nabor_1.count(x) > 1}
 nabor_1 = temp
-
+print(nabor_2)
 print(f'список после удаления дубликатов = {temp}')
